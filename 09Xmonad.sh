d29ccfb197270a01b34846db3b17e230a4d08b79
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-02-03 15:57:47 +0000

    Add files via upload

---
+++ 09Xmonad.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.xmonad
+mkdir -p ~/.xmobar
+
+cd xmonad
+chmod +x autostart.sh
+chmod +x testwallpaper
+cd ..
+
+cd xmobar
+chmod +x dwmcpu
+chmod +x dwmmail
+chmod +x mem.sh
+chmod +x time
+chmod +x time2
+chmod +x updates
+chmod +x weather
+cd ..
+
+cd xmonad
+cp -r * ~/.xmonad
+cd ..
+
+cd xmobar
+cp -r * ~/.xmobar
+cd ..
+
