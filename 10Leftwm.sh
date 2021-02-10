675002670d4068f258b2a4820b4bf388beed462f
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-02-10 16:28:36 +0000

    Add files via upload

---
+++ 10Leftwm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd leftwm
+cd themes
+cd my02-xmobar
+chmod +x change_to_tag
+chmod +x cursor.sh
+chmod +x down
+chmod +x testwallpaper
+chmod +x up
+chmod +x xmobar-config.hs
+
+cd ..
+cd ..
+cd ..
+
+mkdir -p ~/.config/leftwm
+
+cd leftwm
+cp -r * ~/.config/leftwm
+cd ..
