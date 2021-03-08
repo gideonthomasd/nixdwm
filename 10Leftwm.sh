aa0dcc855754a239c80657994868399cf86a63d6
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-03-08 21:53:23 +0000

    Update 10Leftwm.sh

--- 10Leftwm.sh
+++ 10Leftwm.sh
@@ -14,12 +14,65 @@ cd ..
 cd ..
 cd ..
 
+cd leftwm
+cd themes
+cd mypolybar
+chmod +x change_to_tag
+chmod +x cursor.sh
+chmod +x down
+chmod +x testwallpaper
+chmod +x up
+chmod +x launch.sh
+
+cd ..
+cd ..
+cd ..
+
+cd leftwm2
+cd themes
+cd mypolybar
+chmod +x change_to_tag
+chmod +x cursor.sh
+chmod +x down
+chmod +x testwallpaper
+chmod +x up
+chmod +x launch.sh
+
+cd ..
+cd ..
+cd ..
+
+cd leftwm
+cd themes
+cd mypolybar
+cd scripts
+chmod +x *.sh
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd leftwm2
+cd themes
+cd mypolybar
+cd scripts
+chmod +x *.sh
+cd ..
+cd ..
+cd ..
+cd ..
+
 mkdir -p ~/.config/leftwm
+mkdir -p ~/.config/leftwm2
 
 cd leftwm
 cp -r * ~/.config/leftwm
 cd ..
 
+cd leftwm2
+cp -r * ~/.config/leftwm2
+cd ..
+
 cd ~/.config/leftwm/themes
-ln -s my02-xmobar current
+ln -s mypolybar current
 cd ~
