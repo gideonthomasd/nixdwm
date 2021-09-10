ed6a84dcc48f196924049408869c1707bf96239d
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-09-10 19:17:57 +0100

    Add files via upload

--- xmobar/moc.sh
+++ xmobar/moc.sh
@@ -7,11 +7,11 @@ this=""
 state=$(mocp -Q %state 2>/dev/null)
 if [ $state == "PLAY" ]; then
 	icon=" "
-	this=$(mocp -Q %file | sed 's/\//\n/g' | sed '$!d' | awk '$1>0 {print substr($1,1,8)}')
+	this=$(mocp -Q %file | sed 's/\//\n/g' | sed '$!d' | sed 's/ /_/g' | awk '$1>0 {print substr($1,1,15)}' | sed 's/_/ /g')
 	this=$this"..."
 elif [ $state == "PAUSE" ]; then
 	icon=" "
-	this=$(mocp -Q %file | sed 's/\//\n/g' | sed '$!d' | awk '$1>0 {print substr($1,1,8)}')
+	this=$(mocp -Q %file | sed 's/\//\n/g' | sed '$!d' | sed 's/ /_/g' | awk '$1>0 {print substr($1,1,15)}' | sed 's/_/ /g')
 	this=$this"..."
 else
 	icon=" "
